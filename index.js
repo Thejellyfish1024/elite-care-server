47668b3a73150052d320df644c4a2021adf6c717
Thejellyfish1024 <alamin102410@gmail.com>
2023-11-27 23:31:20 +0600

    made get method for user with email

--- index.js
+++ index.js
@@ -61,6 +61,13 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/users/:email', async(req,res) =>{
+      const email = req.params.email;
+      const query = {email : email}
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    })
+
     app.post('/users', async (req, res) => {
       const user = req.body;
       const query = { email: user?.email };
